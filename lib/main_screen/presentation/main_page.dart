e733b310ec6964ec4d38665efa4777e3c3abe8ee
Tylefnx <tayfunucuncu@gmail.com>
2024-12-19 14:45:22 +0300

    Making main page routable for newer version of auto router

--- lib/main_screen/presentation/main_page.dart
+++ lib/main_screen/presentation/main_page.dart
@@ -1,10 +1,12 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:budgetbuddy/main_screen/presentation/categories_widget.dart';
 import 'package:budgetbuddy/main_screen/presentation/logs_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
+@RoutePage()
 class MainPage extends HookWidget {
-  const MainPage({Key? key}) : super(key: key);
+  const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
